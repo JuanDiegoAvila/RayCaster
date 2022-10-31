6b6421509754ca08c59b7241a40253e4c4f7ef9c
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2022-10-31 10:25:41 -0600

    feat: ultimas figuras y generacion de pantalla

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from __future__ import barry_as_FLUFL
 import pygame
+import random
 from OpenGL.GL import *
 
 pygame.init()
@@ -8,7 +8,7 @@ black = (0.0, 0.0, 0.0, 1.0)
 
 size = 4
 width = 100 * size
-height = 100 * size
+height = width
 
 pixels = []
 for i in range(width):
@@ -52,7 +52,7 @@ def Boat(x, y):
     pixels[x+1][y+2] = 1
     pixels[x+2][y+1] = 1
 
-def Tub(x, y):
+def Tube(x, y):
     pixels[x+1][y] = 1
     pixels[x][y+1] = 1
     pixels[x+1][y+2] = 1
@@ -80,6 +80,152 @@ def Blinker(x, y):
     pixels[x][y+1] = 1
     pixels[x][y-1] = 1
 
+def Pulsar(x, y):
+    #izquierda
+    pixels[x-2][y+1] = 1
+    pixels[x-3][y+1] = 1
+    pixels[x-4][y+1] = 1
+    pixels[x-1][y+2] = 1
+    pixels[x-1][y+3] = 1
+    pixels[x-1][y+4] = 1
+
+    pixels[x-6][y+2] = 1
+    pixels[x-6][y+3] = 1
+    pixels[x-6][y+4] = 1
+
+    pixels[x-2][y+6] = 1
+    pixels[x-3][y+6] = 1
+    pixels[x-4][y+6] = 1
+
+    #izquierda abajo
+    pixels[x-2][y-1] = 1
+    pixels[x-3][y-1] = 1
+    pixels[x-4][y-1] = 1
+    pixels[x-1][y-2] = 1
+    pixels[x-1][y-3] = 1
+    pixels[x-1][y-4] = 1
+
+    pixels[x-6][y-2] = 1
+    pixels[x-6][y-3] = 1
+    pixels[x-6][y-4] = 1
+
+    pixels[x-2][y-6] = 1
+    pixels[x-3][y-6] = 1
+    pixels[x-4][y-6] = 1
+
+    #derecha
+    pixels[x+2][y+1] = 1
+    pixels[x+3][y+1] = 1
+    pixels[x+4][y+1] = 1
+    pixels[x+1][y+2] = 1
+    pixels[x+1][y+3] = 1
+    pixels[x+1][y+4] = 1
+
+    pixels[x+6][y+2] = 1
+    pixels[x+6][y+3] = 1
+    pixels[x+6][y+4] = 1
+
+    pixels[x+2][y+6] = 1
+    pixels[x+3][y+6] = 1
+    pixels[x+4][y+6] = 1
+
+    #derecha abajo
+    pixels[x+2][y-1] = 1
+    pixels[x+3][y-1] = 1
+    pixels[x+4][y-1] = 1
+    pixels[x+1][y-2] = 1
+    pixels[x+1][y-3] = 1
+    pixels[x+1][y-4] = 1
+
+    pixels[x+6][y-2] = 1
+    pixels[x+6][y-3] = 1
+    pixels[x+6][y-4] = 1
+
+    pixels[x+2][y-6] = 1
+    pixels[x+3][y-6] = 1
+    pixels[x+4][y-6] = 1
+
+def PentaDecathlon(x, y):
+    pixels[x][y+4] = 1
+    pixels[x-1][y+4] = 1
+    pixels[x+1][y+4] = 1
+    pixels[x-2][y+3] = 1
+    pixels[x+2][y+3] = 1
+    pixels[x-3][y+2] = 1
+    pixels[x+3][y+2] = 1
+    pixels[x-4][y] = 1
+    pixels[x+4][y] = 1
+    pixels[x-4][y-1] = 1
+    pixels[x+4][y-1] = 1
+    pixels[x][y-5] = 1
+    pixels[x-1][y-5] = 1
+    pixels[x+1][y-5] = 1
+    pixels[x-2][y-4] = 1
+    pixels[x+2][y-4] = 1
+    pixels[x-3][y-3] = 1
+    pixels[x+3][y-3] = 1
+
+def Glider(x, y):
+    pixels[x-1][y] = 1
+    pixels[x][y-1] = 1
+    pixels[x+1][y-1] = 1
+    pixels[x+1][y] = 1
+    pixels[x+1][y+1] = 1
+
+def LightSpaceship(x, y):
+    pixels[x][y] = 1
+    pixels[x][y+2] = 1
+    pixels[x+1][y+3] = 1
+    pixels[x+2][y+3] = 1
+    pixels[x+3][y+3] = 1
+    pixels[x+4][y+3] = 1
+    pixels[x+4][y+2] = 1
+    pixels[x+4][y+1] = 1
+    pixels[x+3][y] = 1
+
+def MiddleSpaceship(x, y):
+    pixels[x][y+1] = 1
+    pixels[x][y+3] = 1
+    pixels[x+1][y] = 1
+    pixels[x+2][y] = 1
+    pixels[x+3][y] = 1
+    pixels[x+4][y] = 1
+    pixels[x+5][y] = 1
+    pixels[x+5][y+1] = 1
+    pixels[x+5][y+2] = 1
+    pixels[x+4][y+3] = 1
+    pixels[x+2][y+5] = 1
+
+def Piramid(x, y): # creado
+    pixels[x][y] = 1
+    pixels[x+1][y] = 1
+    pixels[x-1][y] = 1
+    pixels[x-2][y+1] = 1
+    pixels[x-1][y+2] = 1
+    pixels[x][y+3] = 1
+    pixels[x+2][y+1] = 1
+    pixels[x+1][y+2] = 1
+
+def Infinite(x, y):
+    pixels[x][y] = 1
+    pixels[x+1][y+1] = 1
+    pixels[x+1][y-1] = 1
+    pixels[x+2][y+2] = 1
+    pixels[x+2][y-2] = 1
+    pixels[x+3][y+1] = 1
+    pixels[x+3][y-1] = 1
+    pixels[x+4][y] = 1
+
+    
+    pixels[x-1][y+1] = 1
+    pixels[x-1][y-1] = 1
+    pixels[x-2][y+2] = 1
+    pixels[x-2][y-2] = 1
+    pixels[x-3][y+1] = 1
+    pixels[x-3][y-1] = 1
+    pixels[x-4][y] = 1
+
+
 def pixel(x, y, color):
     glEnable(GL_SCISSOR_TEST)
     glScissor(x, y, 10, 10)
@@ -103,31 +249,29 @@ def GameOfLife(pixels):
         for j in range(height):
             # cada pixel individual
             vecinos = 0
-            try:
-                if pixels[i][j+1] == 1 or pixels[i][j+1] == 2:
-                    vecinos += 1
-                if pixels[i][j-1] == 1 or pixels[i][j-1] == 2:
-                    vecinos += 1
-                if pixels[i-1][j-1] == 1 or pixels[i-1][j-1] == 2:
-                    vecinos += 1
-                if pixels[i+1][j-1] == 1 or pixels[i+1][j-1] == 2:
-                    vecinos += 1
-                if pixels[i-1][j+1] == 1 or pixels[i-1][j+1] == 2:
-                    vecinos += 1
-                if pixels[i+1][j+1] == 1 or pixels[i+1][j+1] == 2:
-                    vecinos += 1
-                if pixels[i-1][j] == 1 or pixels[i-1][j] == 2:
-                    vecinos += 1
-                if pixels[i+1][j] == 1 or pixels[i+1][j] == 2:
-                    vecinos += 1
 
-                if pixels[i][j] == 1:
-                    if vecinos < 2 or vecinos > 3:
-                        pixels[i][j] = 2
-                elif vecinos == 3:
-                    pixels[i][j] = 3
-            except:
-                pass
+            if pixels[i][(j+1)%height] == 1 or pixels[i][(j+1)%height] == 2:
+                vecinos += 1
+            if pixels[i][j-1] == 1 or pixels[i][j-1] == 2:
+                vecinos += 1
+            if pixels[i-1][j-1] == 1 or pixels[i-1][j-1] == 2:
+                vecinos += 1
+            if pixels[(i+1)%width][j-1] == 1 or pixels[(i+1)%width][j-1] == 2:
+                vecinos += 1
+            if pixels[i-1][(j+1)%height] == 1 or pixels[i-1][(j+1)%height] == 2:
+                vecinos += 1
+            if pixels[(i+1)%width][(j+1)%height] == 1 or pixels[(i+1)%width][(j+1)%height] == 2:
+                vecinos += 1
+            if pixels[i-1][j] == 1 or pixels[i-1][j] == 2:
+                vecinos += 1
+            if pixels[(i+1)%width][j] == 1 or pixels[(i+1)%width][j] == 2:
+                vecinos += 1
+
+            if pixels[i][j] == 1:
+                if vecinos < 2 or vecinos > 3:
+                    pixels[i][j] = 2
+            elif vecinos == 3:
+                pixels[i][j] = 3
     
     for i in range(width):
         for j in range(height):
@@ -138,8 +282,50 @@ def GameOfLife(pixels):
             
     return pixels
 
+def GenerateScreen(factor):
+    for i in range(width):
+        for j in range(height):
 
-Beacon(10, 10)
+            if j % factor == 1 and i % factor == 1 and j > 10 and i > 10:
+                
+                probabilidad = random.randint(1, 10)
+                if probabilidad >= 6:
+                    x = int(i/size)
+                    y = int(j/size)
+                    func = random.randint(1, 15)
+
+                    if func == 1:
+                        Block(x, y)
+                    elif func == 2:
+                        BeeHive(x, y)
+                    elif func == 3:
+                        Loaf(x, y)
+                    elif func == 4:
+                        Boat(x, y)
+                    elif func == 5:
+                        Tube(x, y)
+                    elif func == 6:
+                        Toad(x, y)
+                    elif func == 7:
+                        Beacon(x, y)
+                    elif func == 8:
+                        Blinker(x, y)
+                    elif func == 9:
+                        Pulsar(x, y)
+                    elif func == 10:
+                        PentaDecathlon(x, y)
+                    elif func == 11:
+                        Glider(x, y)
+                    elif func == 12:
+                        LightSpaceship(x, y)
+                    elif func == 13:
+                        MiddleSpaceship(x, y)
+                    elif func == 14:
+                        Piramid(x, y)
+                    elif func == 15:
+                        Infinite(x, y)
+        
+GenerateScreen(int((width/size)/2))
 running = True
 while running:
     # clean
@@ -156,4 +342,4 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # break
+    #break
